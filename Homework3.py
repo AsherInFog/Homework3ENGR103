fcecaf735a3fb3cc9114dbe4a47db79173f23def
Asher Charlton <asherbc@Ashers-MacBook-Pro.local>
2026-02-01 21:48:29 -0800

    updated homework3

--- Homework3.py
+++ Homework3.py
@@ -6,33 +6,88 @@
 # Input: Varied
 # Output: Varied
 ##########################################################################
+import math
 
 def welcome_msg():
     print("{===============================================}")
     print("  Welcome to Asher's Quality Control Program!")
     print("{===============================================}")
 
-welcome_msg()
-
-def measure_of_int():
+def moi_sample():
     interest = []
     for i in range(1, 6):
         while True:
             try:
-                val = float(input(f"Please enter the value for measure {i} (decimal): "))
+                val = float(input(f"Please enter the value for measure {i}: "))
             except ValueError:
-                print("Invalid input. Please enter a decimal number.")
+                print("\nInvalid input. Please enter a NUMBER.")
                 continue
             interest.append(val)
             break
 
     return interest
 
-def sample_calc():
-    values = measure_of_int()
+
+# Function that calculates the average sample
+def avg_sample_calc(values):
     avg = sum(values) / len(values)
-    print(f"Average of the {len(values)} measures: {avg:.2f}")
     return avg
 
-sample_calc()
+
+def moi_goal_average():
+    while True:
+        try:
+            moi = float(input("Please input the measure of interest's goal average: "))
+            if moi <= 0:
+                print("\nPlease enter in a nunber greater than 0")
+                continue
+        except ValueError:
+            print("\nInvalid Input. Please enter a NUMBER")
+            continue
+        break
+    return moi
     
+
+def goal_standard_deviation():
+    while True:
+        try:
+            gsd = float(input("Please enter the goal standard deviation: "))
+            if gsd < 0:
+                print("\nPlease enter in a posive number")
+                continue
+        except ValueError:
+            print("\nInvalid input. Please enter a NUMBER")
+            continue
+        break
+    return gsd
+            
+
+def lowerbound_calc(moi, gsd, n):
+    lower = moi - ((3 * gsd)/math.sqrt(n))
+    return lower
+
+def upperbound_calc(moi, gsd, n):
+    upper = moi + ((3 * gsd)/math.sqrt(n))
+    return upper
+
+
+def main():
+    welcome_msg()
+   
+    interest = moi_sample()
+    avg = avg_sample_calc(interest)
+    moi = moi_goal_average()
+    gsd = goal_standard_deviation()
+
+    n = len(interest)
+    lower = lowerbound_calc(moi, gsd, n)
+    upper = upperbound_calc(moi, gsd, n)
+
+    print(f"\n\nYour lower bound is {lower:.2f}")
+    print(f"Your average is {avg:.2f}")
+    print(f"Your upper bound is {upper:.2f}")
+    print("\n\nThank you for using my program!")
+
+main()
+
+
